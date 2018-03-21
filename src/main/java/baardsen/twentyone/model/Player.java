79392f3dc6e8a1c64ea17994609937f83e896fde
Svein Baardsen <svein.baardsen@gmail.com>
2018-03-21 23:31:40 +0100

    Added logic for when a Player wants another card

--- src/main/java/baardsen/twentyone/model/Player.java
+++ src/main/java/baardsen/twentyone/model/Player.java
@@ -3,16 +3,43 @@ package baardsen.twentyone.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public abstract class Player {
 
 	private final List<Card> cards = new ArrayList<>();
 
-	public static Player dealer() {
-		return new Player();
+	public static Player dealer(Player opponent) {
+		return new Dealer(opponent);
 	}
 
 	public static Player sam() {
-		return new Player();
+		return new Sam();
+	}
+
+	public abstract boolean hitMe();
+
+	private static class Dealer extends Player {
+
+		private final Player opponent;
+
+		public Dealer(Player opponent) {
+			this.opponent = opponent;
+		}
+
+		@Override
+		public boolean hitMe() {
+			return !isBust() &&
+					score() <= opponent.getScore();
+		}
+
+	}
+
+	private static class Sam extends Player {
+
+		@Override
+		public boolean hitMe() {
+			return score() < 17;
+		}
+
 	}
 
 	public void addCard(Card card) {
@@ -23,11 +50,11 @@ public class Player {
 		return isBust() ? 0 : score();
 	}
 
-	public boolean isBust() {
+	protected boolean isBust() {
 		return score() > 21;
 	}
 
-	private int score() {
+	protected int score() {
 		return cards.stream()
 				.mapToInt(Card::getValue)
 				.sum();
--- src/test/java/baardsen/twentyone/model/PlayerTest.java
+++ src/test/java/baardsen/twentyone/model/PlayerTest.java
@@ -46,4 +46,109 @@ class PlayerTest {
 		assertTrue(player.isBust());
 	}
 
+	@Test
+	public void hitMe_samWithScoreOf16_true() {
+		var player = Player.sam();
+		player.addCard(Card.of(Suit.SPADE, Rank.KING));
+		player.addCard(Card.of(Suit.SPADE, Rank.SIX));
+
+		assertEquals(16, player.getScore());
+		assertTrue(player.hitMe());
+	}
+
+	@Test
+	public void hitMe_samWithScoreOf17_false() {
+		var player = Player.sam();
+		player.addCard(Card.of(Suit.SPADE, Rank.KING));
+		player.addCard(Card.of(Suit.SPADE, Rank.SEVEN));
+
+		assertEquals(17, player.getScore());
+		assertFalse(player.hitMe());
+	}
+
+	@Test
+	public void hitMe_dealerWithLowerScoreThanSam_true() {
+		var sam = Player.sam();
+		sam.addCard(Card.of(Suit.SPADE, Rank.KING));
+		sam.addCard(Card.of(Suit.SPADE, Rank.EIGHT));
+
+		assertEquals(18, sam.getScore());
+
+		var dealer = Player.dealer(sam);
+		dealer.addCard(Card.of(Suit.SPADE, Rank.KING));
+		dealer.addCard(Card.of(Suit.SPADE, Rank.SIX));
+
+		assertEquals(16, dealer.getScore());
+
+		assertTrue(dealer.hitMe());
+	}
+
+	@Test
+	public void hitMe_dealerWithHigherScoreThanSam_false() {
+		var sam = Player.sam();
+		sam.addCard(Card.of(Suit.SPADE, Rank.KING));
+		sam.addCard(Card.of(Suit.SPADE, Rank.EIGHT));
+
+		assertEquals(18, sam.getScore());
+
+		var dealer = Player.dealer(sam);
+		dealer.addCard(Card.of(Suit.SPADE, Rank.KING));
+		dealer.addCard(Card.of(Suit.SPADE, Rank.JACK));
+
+		assertEquals(20, dealer.getScore());
+
+		assertFalse(dealer.hitMe());
+	}
+
+	@Test
+	public void hitMe_dealerWhoIsBust_false() {
+		var sam = Player.sam();
+		sam.addCard(Card.of(Suit.SPADE, Rank.KING));
+		sam.addCard(Card.of(Suit.SPADE, Rank.EIGHT));
+
+		assertEquals(18, sam.getScore());
+
+		var dealer = Player.dealer(sam);
+		dealer.addCard(Card.of(Suit.SPADE, Rank.KING));
+		dealer.addCard(Card.of(Suit.SPADE, Rank.SIX));
+		dealer.addCard(Card.of(Suit.SPADE, Rank.TEN));
+
+		assertTrue(dealer.isBust());
+
+		assertFalse(dealer.hitMe());
+	}
+
+	@Test
+	public void hitMe_dealerWhenSamIsBust_false() {
+		var sam = Player.sam();
+		sam.addCard(Card.of(Suit.SPADE, Rank.KING));
+		sam.addCard(Card.of(Suit.SPADE, Rank.SIX));
+		sam.addCard(Card.of(Suit.SPADE, Rank.EIGHT));
+
+		assertTrue(sam.isBust());
+
+		var dealer = Player.dealer(sam);
+		dealer.addCard(Card.of(Suit.SPADE, Rank.KING));
+		dealer.addCard(Card.of(Suit.SPADE, Rank.TEN));
+
+		assertFalse(dealer.hitMe());
+	}
+
+	@Test
+	public void hitMe_dealerWhoHasTheSameScoreAsSam_true() {
+		var sam = Player.sam();
+		sam.addCard(Card.of(Suit.SPADE, Rank.ACE));
+		sam.addCard(Card.of(Suit.SPADE, Rank.EIGHT));
+
+		assertEquals(19, sam.getScore());
+
+		var dealer = Player.dealer(sam);
+		dealer.addCard(Card.of(Suit.HEART, Rank.ACE));
+		dealer.addCard(Card.of(Suit.HEART, Rank.EIGHT));
+
+		assertEquals(19, dealer.getScore());
+
+		assertTrue(dealer.hitMe());
+	}
+
 }
